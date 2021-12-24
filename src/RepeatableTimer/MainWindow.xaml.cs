c0bafdc0d3f562a742270e5dd6c8f297b1fe039f
SIkebe <SIkebe@users.noreply.github.com>
2021-12-24 19:14:13 +0900

    Format

--- src/RepeatableTimer/MainWindow.xaml.cs
+++ src/RepeatableTimer/MainWindow.xaml.cs
@@ -1,17 +1,16 @@
-﻿using System.Windows;
+using System.Windows;
 using RepeatableTimer.ViewModels;
 
-namespace RepeatableTimer
+namespace RepeatableTimer;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-            DataContext = new MainWindowViewModel();
-        }
+        InitializeComponent();
+        DataContext = new MainWindowViewModel();
     }
 }
